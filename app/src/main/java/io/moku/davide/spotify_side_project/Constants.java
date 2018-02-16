1f2c2c52a3202bea181932418a834c478718b4ea
Davide Castello <davide@moku.io>
2018-02-16 16:16:26 +0100

    first commit

---
+++ app/src/main/java/io/moku/davide/spotify_side_project/Constants.java
@@ -0,0 +1,12 @@
+package io.moku.davide.spotify_side_project;
+
+/**
+ * Created by Davide Castello on 16/02/18.
+ * Project: spotify-side-project
+ * Copyright © 2018 Moku S.r.l. All rights reserved.
+ */
+
+public class Constants {
+
+    public static final String CLIENT_ID = "9ecd6e858f1648f98a6f90589b866ee3";
+}
