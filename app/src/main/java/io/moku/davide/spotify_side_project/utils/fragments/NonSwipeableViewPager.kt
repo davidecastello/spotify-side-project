168c22a11f1bf3458b106156066d131038dc784c
Davide Castello <davide@moku.io>
2018-05-30 16:38:00 +0200

    removed swipe between app sections

---
+++ app/src/main/java/io/moku/davide/spotify_side_project/utils/fragments/NonSwipeableViewPager.kt
@@ -0,0 +1,56 @@
+package io.moku.davide.spotify_side_project.utils.fragments
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import java.lang.reflect.Field
+
+/**
+ * Created by Davide Castello on 30/05/18.
+ * Project: spotify-side-project
+ * Copyright © 2018 Moku S.r.l. All rights reserved.
+ */
+class NonSwipeableViewPager : ViewPager {
+
+    constructor(context: Context) : super(context) {
+        setMyScroller()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setMyScroller()
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    //down one is added for smooth scrolling
+
+    private fun setMyScroller() {
+        try {
+            val viewpager = ViewPager::class.java
+            val scroller = viewpager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this, MyScroller(context))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    inner class MyScroller(context: Context) : Scroller(context, DecelerateInterpolator()) {
+
+        override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+            super.startScroll(startX, startY, dx, dy, 200 /*1 secs*/)
+        }
+    }
+}
